aa4570737979af1bc3ef42ca283ae92fe4c3b7c1
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-15 09:51:34 +0200

    Added controller and route definition

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var controller = require("../controller/fitnessController");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post("/workouts", controller.postWorkout);
+router.post("/exercises", controller.postExercise);
+router.post("/logs", controller.postLog);
+
+router.get("/workouts", controller.getWorkout);
+router.get("/exercises", controller.getExercise);
+router.get("/logs", controller.getLog);
 
 module.exports = router;
