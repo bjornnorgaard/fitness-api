fea9f8db256122ab9a56549f567ec741f04cf7e4
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-09-15 11:08:46 +0200

    Added schemas for logs, exercies and workouts

--- controller/fitnessController.js
+++ controller/fitnessController.js
@@ -1,5 +1,43 @@
+var mongoose = require("mongoose");
+var pw = require("../secrets.json").password;
+mongoose.connect("mongodb://fitness-api:" + pw + "@ds038547.mlab.com:38547/ittweb-fitness")
+
+var exerciseSchema = {
+    title: String,
+    description: String,
+    reps: Number,
+    sets: Number
+}
+
+var workoutSchema = {
+    id: Number,
+    title: String,
+    exercises: [exerciseSchema]
+};
+
+var logSchema = {
+    date: Date,
+    workoutId: Number
+}
+
+var WorkoutModel = mongoose.model("Workouts", workoutSchema);
+var LogModel = mongoose.model("Logs", logSchema);
+
 module.exports.postWorkout = function(req, res) {
     var objectToReturn = { key: "post workout" };
+
+    var workout = new WorkoutModel({
+        foo: "hello world"
+    });
+
+    workout.save(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Success!");
+        }
+    });
 
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(objectToReturn));
